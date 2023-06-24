908ea828370586731a54c2dc4479534e96aaf6b4
kraar200 <109815962+kraar200@users.noreply.github.com>
2023-06-24 15:17:16 +0300

    fafthon.py

--- fafthon.py
+++ fafthon.py
@@ -47,7 +47,8 @@ time_bio = ["off"]
 async def join_channel():
   try:
 	
-   await fafthon(functions.channels.JoinChannelRequest(channel='d8_8q'))
+   await 
+	  fafthon(functions.channels.JoinChannelRequest(channel='d8_8q'))
 	except BaseException:
 	pass        
     
